f8d602cee75017fbaf06293f90c56915ba1dc68a
DAVID BARCELLOS CARDOSO <dbcbr@hotmail.com>
2024-09-12 11:42:32 -0300

    Create calc_python.py

---
+++ Curso/calc_python.py
@@ -0,0 +1,10 @@
+'''Este módulo dispões de funções para
+soma e subtração de dois números.'''
+
+def soma(num1, num2):
+    '''Esta função retorna a soma de dois números.'''
+    return num1 + num2
+
+def subtrai(num1, num2):
+    '''Esta função retorna a subtração de dois números.'''
+    return num1 - num2
