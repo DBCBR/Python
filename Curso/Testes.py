f0037ecc9b992a2e806b8b4fc95f81b8e3d7e4db
DAVID BARCELLOS CARDOSO <dbcbr@hotmail.com>
2024-09-24 11:06:21 -0300

    Update Testes.py

--- Curso/Testes.py
+++ Curso/Testes.py
@@ -5,4 +5,27 @@ import pytest
 
 # pytest.main(["-m", "primarios", "-v","Curso/teste_1.py", "Curso/teste_2.py"])
 
-pytest.main(["-k", "test_par3", "-v", 'Curso/teste_1.py'])
+# pytest.main(["-k", "test_par3", "-v", 'Curso/teste_1.py'])
+
+# from primo import primo
+# print(primo(3))
+# print(primo(4))
+# print(primo(5))
+# print(primo(6))
+# print(primo(7))
+# print(primo(8))
+
+# pytest.main(["-v", "Curso/test_primo.py"])
+
+# from primo import primo
+# print(primo(-1))
+# print(primo(1))
+# print(primo(2))
+# print(primo(3))
+# print(primo(4))
+# print(primo(5))
+# print(primo(6))
+# print(primo(7))
+# print(primo(8))
+
+pytest.main(["-v", "Curso/test_primo.py"])
