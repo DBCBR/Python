01b07ec21cfcd5ab4d8646449cc9e79a5647baea
DAVID BARCELLOS CARDOSO <dbcbr@hotmail.com>
2024-09-26 11:34:42 -0300

    Update cliente.py

--- Curso/cliente.py
+++ Curso/cliente.py
@@ -1,4 +1,4 @@
 import requests
-x = requests.get('http://127.0.0.1:5000/tabela/')
+x = requests.get("http://ec2-54-207-131-39.sa-east-1.compute.amazonaws.com:5003/cotacaocompleta?valor=10&mes=Janeiro")
 print(x.text)
 
