a87e268b67299f55361842540612c3e43fbbd398
DAVID BARCELLOS CARDOSO <dbcbr@hotmail.com>
2024-09-27 09:34:00 -0300

    Delete Banco de Dados.py

--- Banco de Dados.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-con = psycopg2.connect(host='database-1.c9o20cogif9e.sa-east-1.rds.amazonaws.com', database='inventario', user='postgres', password='123teste4')
-con.autocommit = True
-cur = con.cursor()
-cur.execute("delete from arquivo")
-con.commit()
-
-con.close()
